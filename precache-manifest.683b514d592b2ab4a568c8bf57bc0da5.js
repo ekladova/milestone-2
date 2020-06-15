3161a9ade5a66b823f89ca5fa0ff8a29daf4f87f
Kate Kladova <ek.kladova@gmail.com>
2020-06-15 14:59:22 +0100

    Updates

---
+++ precache-manifest.683b514d592b2ab4a568c8bf57bc0da5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e750e40bb1fdf9f4ba3f7785fb108b",
+    "url": "/milestone-2/index.html"
+  },
+  {
+    "revision": "36acd560b419666c5fe1",
+    "url": "/milestone-2/static/css/main.bb2dab60.chunk.css"
+  },
+  {
+    "revision": "a85fe320a4de76f5ed13",
+    "url": "/milestone-2/static/js/2.3e86bf9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/milestone-2/static/js/2.3e86bf9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36acd560b419666c5fe1",
+    "url": "/milestone-2/static/js/main.4bb0260c.chunk.js"
+  },
+  {
+    "revision": "9834560be99075c1fb61",
+    "url": "/milestone-2/static/js/runtime-main.ce620bdb.js"
+  },
+  {
+    "revision": "e6f623ce8929666d12bb37dc6d4de448",
+    "url": "/milestone-2/static/media/recording-dot-png.e6f623ce.png"
+  },
+  {
+    "revision": "da21e594891c8bc27aa4cd129fb5976b",
+    "url": "/milestone-2/static/media/tom-go.da21e594.png"
+  }
+]);
